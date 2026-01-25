03360ba30c3b4cbe745b9b72830202ab9cbc253f
Neres <neresfjcomunic@gmail.com>
2026-01-24 21:47:58 -0300

    feat: atualiza layout da pagina principal

--- pages/index.js
+++ pages/index.js
@@ -16,46 +16,57 @@ const HomePage = () => {
           <br />
           Um espaço para ideias, tecnologia e conteúdo.
         </p>
-
-        <div className={styles.progressWrapper}>
+        <h2>
+          Stem Verse é um hub local que divulga projetos e iniciativas que
+          aproximam alunos de escolas públicas e privadas do universo STEM.
+        </h2>
+        {/* <div className={styles.progressWrapper}>
           <span>0%</span>
           <div className={styles.progressBar}>
             <div className={styles.progress} />
           </div>
           <span>100%</span>
-        </div>
+        </div> */}
+        <h3 className={styles.tagline}>Construindo conexões</h3>
 
         <button className={styles.button} disabled>
           Em breve
         </button>
 
-        <footer className={styles.social}>
-          <a
-            href="https://www.instagram.com/oneres_stm"
-            target="_blank"
-            rel="noreferrer"
-            aria-label="Instagram"
-          >
-            <FaInstagram />
-          </a>
+        <footer>
+          <div className={styles.social}>
+            <a
+              href="https://www.instagram.com/oneres_stm"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Instagram"
+            >
+              <FaInstagram />
+            </a>
 
-          <a
-            href="https://www.linkedin.com/in/fabioneresdejesus"
-            target="_blank"
-            rel="noreferrer"
-            aria-label="LinkedIn"
-          >
-            <FaLinkedinIn />
-          </a>
+            <a
+              href="https://www.linkedin.com/in/fabioneresdejesus"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedinIn />
+            </a>
 
-          <a
-            href="https://www.youtube.com/@mocorongotech"
-            target="_blank"
-            rel="noreferrer"
-            aria-label="YouTube"
-          >
-            <FaYoutube />
-          </a>
+            <a
+              href="https://www.youtube.com/@mocorongotech"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="YouTube"
+            >
+              <FaYoutube />
+            </a>
+          </div>
+          <p className={styles.location}>
+            Santarém - PA
+            <br />
+            Desenvolvido por EngVerse.
+          </p>
         </footer>
       </section>
     </main>
